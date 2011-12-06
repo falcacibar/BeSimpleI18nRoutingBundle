a7e82d324dbcacd9412ddc45030b8ef65ac3a3a4
Sebastián Vicencio <sivicencio@gmail.com>
2011-12-06 01:26:45 -0300

    Primeras recomendaciones y edad funcionan bien. Vamos por el registro ahora.

--- src/Loogares/UsuarioBundle/Controller/UsuarioController.php
+++ src/Loogares/UsuarioBundle/Controller/UsuarioController.php
@@ -28,33 +28,11 @@ class UsuarioController extends Controller
         $totalRecomendaciones = count($recomendaciones);
 
         //Primeras recomendaciones usuario
-        $primerasRecomendaciones = 0;
-        $primRec  = array();
-        foreach($recomendaciones as $r) {
-            if($pr->verificarPrimeroRecomendar($usuarioResult->getId(), $r->getLugar()->getId())) {
-                $primerasRecomendaciones++;
-            }
-        }
-
-<<<<<<< HEAD
-        $resultado = $this->getDoctrine()->getConnection()->fetchAll("SELECT *
-                               FROM recomendacion r
-                               INNER JOIN lugares l
-                               ON l.id = r.lugar_id
-                               WHERE r.lugar_id = 2487
-                               ORDER BY r.fecha_creacion ASC");
-        
+        $primerasRecomendaciones = $pr->getPrimerasRecomendaciones($usuarioResult->getId());
+        $totalPrimerasRecomendaciones = count($primerasRecomendaciones);   
 
         //Total de lugares agregados por el usuario
         $totalLugaresAgregados = $pr->getLugaresAgregadosUsuario($usuarioResult->getId());
-=======
-        //Query para obtener el total de lugares agregados por el usuario
-        $q = $em->createQuery("SELECT COUNT(l) total 
-                               FROM Loogares\LugarBundle\Entity\Lugar l 
-                               WHERE l.usuario_id = ?1");
-        $q->setParameter(1, $usuarioResult->getId());
-        $totalLugaresAgregados = $q->getSingleResult();
->>>>>>> ae5d540846abfe01d5a7c50a8e404cd2eef8c1a2
 
         //Total de fotos de lugares agregadas por el usuario
         $totalImagenesLugar= $pr->getFotosLugaresAgregadasUsuario($usuarioResult->getId());
@@ -62,10 +40,15 @@ class UsuarioController extends Controller
         //Cálculo de edad
         if($usuarioResult->getFechaNacimiento() != null) {
             $birthday = $usuarioResult->getFechaNacimiento()->format('d-m-Y');
-            list($d,$m,$Y)    = explode("-",$birthday);
-            $edad = date("md") < $m.$d ? date("Y")-$Y-1 : date("Y")-$Y;
+            if($birthday != '30-11--0001') {
+                list($d,$m,$Y)    = explode("-",$birthday);
+                $edad = date("md") < $m.$d ? date("Y")-$Y-1 : date("Y")-$Y;
+            }
+            else {
+                $edad = '0';
+            }            
         } else {
-            $edad = null;
+            $edad = '0';
         }
         
 
@@ -98,14 +81,13 @@ class UsuarioController extends Controller
          */
         $data = $usuarioResult;
         $data->totalRecomendaciones = $totalRecomendaciones;
-        $data->totalPrimerasRecomendaciones = $primerasRecomendaciones;
+        $data->totalPrimerasRecomendaciones = $totalPrimerasRecomendaciones;
         $data->totalLugaresAgregados = $totalLugaresAgregados['total'];
         $data->totalImagenesLugar = $totalImagenesLugar['total'];
         $data->edadResult = $edad;
         $data->sexoResult = $sexoResult;
         $data->desdeResult = $usuarioResult->getFechaRegistro()->format('d-m-Y');
         $data->links = $links;
-        $data->resultado = $resultado;
         return $this->render('LoogaresUsuarioBundle:Usuarios:show.html.twig', array('usuario' => $data));  
     }
 }
