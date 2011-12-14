ee31e43eaa27d78342321f66cb9cecec2b989f08
Sebastián Vicencio <sivicencio@gmail.com>
2011-12-14 19:05:37 -0300

    Actualizo vendors, y funciona el comienzo de la integración con Facebook. El resto se probará más adelante en el servidor

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new FOS\FacebookBundle\FOSFacebookBundle(),
             new Loogares\LugarBundle\LoogaresLugarBundle(),
             new Loogares\UsuarioBundle\LoogaresUsuarioBundle(),
             new Loogares\ExtraBundle\LoogaresExtraBundle(),
