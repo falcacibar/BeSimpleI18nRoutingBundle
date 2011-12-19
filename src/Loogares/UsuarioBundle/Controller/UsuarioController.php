789471964f00044d18a4e7c1d6874c63c570fb11
Sebastián Vicencio <sivicencio@gmail.com>
2011-12-19 12:31:25 -0300

    Links en header para login y registro, y avances en perfil de usuario

--- src/Loogares/UsuarioBundle/Controller/UsuarioController.php
+++ src/Loogares/UsuarioBundle/Controller/UsuarioController.php
@@ -18,6 +18,12 @@ class UsuarioController extends Controller
     }
     
     public function showAction($param) {
+        
+        return $this->forward('LoogaresUsuarioBundle:Usuario:actividad', array('param' => $param));
+          
+    }
+
+    public function actividadAction($param) {
         $em = $this->getDoctrine()->getEntityManager();
         $ur = $em->getRepository("LoogaresUsuarioBundle:Usuario");
         
@@ -26,71 +32,22 @@ class UsuarioController extends Controller
             throw $this->createNotFoundException('No existe usuario con el id/username: '.$param);
         }
         
-        //Total recomendaciones usuario
-        $recomendaciones = $ur->getUsuarioRecomendaciones($usuarioResult->getId()); 
-        $totalRecomendaciones = count($recomendaciones);
+        $data = $ur->getDatosUsuario($usuarioResult);
+        $data->tipo = 'actividad';
+        return $this->render('LoogaresUsuarioBundle:Usuarios:show.html.twig', array('usuario' => $data));  
+    }
 
-        //Primeras recomendaciones usuario
-        $primerasRecomendaciones = $ur->getPrimerasRecomendaciones($usuarioResult->getId());
-        $totalPrimerasRecomendaciones = count($primerasRecomendaciones);   
-
-        //Total de lugares agregados por el usuario
-        $totalLugaresAgregados = $ur->getLugaresAgregadosUsuario($usuarioResult->getId());
-
-        //Total de fotos de lugares agregadas por el usuario
-        $totalImagenesLugar= $ur->getFotosLugaresAgregadasUsuario($usuarioResult->getId());
-
-        //Cálculo de edad
-        if($usuarioResult->getFechaNacimiento() != null) {
-            $birthday = $usuarioResult->getFechaNacimiento()->format('d-m-Y');
-            if($birthday != '30-11--0001') {
-                list($d,$m,$Y)    = explode("-",$birthday);
-                $edad = date("md") < $m.$d ? date("Y")-$Y-1 : date("Y")-$Y;
-            }
-            else {
-                $edad = '0';
-            }            
-        } else {
-            $edad = '0';
+    public function recomendacionesAction($param) {
+        $em = $this->getDoctrine()->getEntityManager();
+        $ur = $em->getRepository("LoogaresUsuarioBundle:Usuario");
+        
+        $usuarioResult = $ur->findOneByIdOrSlug($param);
+        if(!$usuarioResult) {
+            throw $this->createNotFoundException('No existe usuario con el id/username: '.$param);
         }
         
-
-        //Nombre del sexo
-        if($usuarioResult->getSexo() != null) {
-            if($usuarioResult->getSexo() == "m") {
-                $sexoResult = "Hombre";
-            }
-            else {
-                $sexoResult = "Mujer";
-            }
-        } else {
-            $sexoResult = null;
-        }
-
-        //Array con links de usuario
-        $links = array();
-        if($usuarioResult->getWeb() != null || $usuarioResult->getWeb() != '') {
-            $links[] = $usuarioResult->getWeb();
-        }
-        if($usuarioResult->getFacebook() != null || $usuarioResult->getFacebook() != '') {
-            $links[] = $usuarioResult->getFacebook();
-        }
-        if($usuarioResult->getTwitter() != null || $usuarioResult->getTwitter() != '') {
-            $links[] = $usuarioResult->getTwitter();
-        }
-        
-        /*
-         *  Armado de Datos para pasar a Twig
-         */
-        $data = $usuarioResult;
-        $data->totalRecomendaciones = $totalRecomendaciones;
-        $data->totalPrimerasRecomendaciones = $totalPrimerasRecomendaciones;
-        $data->totalLugaresAgregados = $totalLugaresAgregados['total'];
-        $data->totalImagenesLugar = $totalImagenesLugar['total'];
-        $data->edadResult = $edad;
-        $data->sexoResult = $sexoResult;
-        $data->desdeResult = $usuarioResult->getFechaRegistro()->format('d-m-Y');
-        $data->links = $links;
+        $data = $ur->getDatosUsuario($usuarioResult);
+        $data->tipo = 'recomendaciones';
         return $this->render('LoogaresUsuarioBundle:Usuarios:show.html.twig', array('usuario' => $data));  
     }
 
--- src/Loogares/UsuarioBundle/Repository/UsuarioRepository.php
+++ src/Loogares/UsuarioBundle/Repository/UsuarioRepository.php
@@ -49,55 +49,122 @@ class UsuarioRepository extends EntityRepository implements UserProviderInterfac
     }
 
   	public function getUsuarioRecomendaciones($id) {
-  		$em = $this->getEntityManager();
+  		  $em = $this->getEntityManager();
 
-  		//Query para obtener el total de recomendaciones del usuario
-          $q = $em->createQuery("SELECT r
-                                 FROM Loogares\UsuarioBundle\Entity\Recomendacion r 
-                                 WHERE r.usuario = ?1");
-          $q->setParameter(1, $id);
+  		  //Query para obtener el total de recomendaciones del usuario
+        $q = $em->createQuery("SELECT r
+                               FROM Loogares\UsuarioBundle\Entity\Recomendacion r 
+                               WHERE r.usuario = ?1");
+        $q->setParameter(1, $id);
 
-          return $q->getResult();
+        return $q->getResult();
   	}
 
-  	public function getLugaresAgregadosUsuario($id) {
-  		$em = $this->getEntityManager();
+  	public function getTotalLugaresAgregadosUsuario($id) {
+    		$em = $this->getEntityManager();
 
-  		 //Query para obtener el total de lugares agregados por el usuario
-          $q = $em->createQuery("SELECT COUNT(l) total 
-                                 FROM Loogares\LugarBundle\Entity\Lugar l 
-                                 WHERE l.usuario = ?1");
-          $q->setParameter(1, $id);
-          return $q->getSingleResult();
+    		//Query para obtener el total de lugares agregados por el usuario
+        $q = $em->createQuery("SELECT COUNT(l) total 
+                               FROM Loogares\LugarBundle\Entity\Lugar l 
+                               WHERE l.usuario = ?1");
+        $q->setParameter(1, $id);
+        return $q->getSingleResult();
   	}
 
-  	public function getFotosLugaresAgregadasUsuario($id) {
-  		$em = $this->getEntityManager();
+  	public function getTotalFotosLugaresAgregadasUsuario($id) {
+    		$em = $this->getEntityManager();
 
-  		//Query para obtener el total de fotos de lugares agregadas por el usuario
-          $q = $em->createQuery("SELECT COUNT(im) total 
-                                 FROM Loogares\LugarBundle\Entity\ImagenLugar im 
-                                 WHERE im.usuario = ?1");
-          $q->setParameter(1, $id);
-          return $q->getSingleResult();
+    		//Query para obtener el total de fotos de lugares agregadas por el usuario
+        $q = $em->createQuery("SELECT COUNT(im) total 
+                               FROM Loogares\LugarBundle\Entity\ImagenLugar im 
+                               WHERE im.usuario = ?1");
+        $q->setParameter(1, $id);
+        return $q->getSingleResult();
   	}
 
   	public function getPrimerasRecomendaciones($id) {
-  		$em = $this->getEntityManager();
+    		$em = $this->getEntityManager();
 
-  		//Verificamos si usuario es el primero en recomendar en el lugar
-  		$q = $em->createQuery("SELECT l.nombre, r1.texto 
-  							   FROM LoogaresUsuarioBundle:Recomendacion r1 
-  							   JOIN r1.lugar l
-  							   WHERE r1.usuario = ?1
-  							   AND r1.fecha_creacion = (SELECT MIN(r2.fecha_creacion) 
-  							   FROM LoogaresUsuarioBundle:Recomendacion r2 
-  							   WHERE r2.lugar = r1.lugar)");
-  		$q->setParameter(1,$id);
-  		return $q->getResult();
+    		//Verificamos si usuario es el primero en recomendar en el lugar
+    		$q = $em->createQuery("SELECT l.nombre, r1.texto 
+    							   FROM LoogaresUsuarioBundle:Recomendacion r1 
+    							   JOIN r1.lugar l
+    							   WHERE r1.usuario = ?1
+    							   AND r1.fecha_creacion = (SELECT MIN(r2.fecha_creacion) 
+    							   FROM LoogaresUsuarioBundle:Recomendacion r2 
+    							   WHERE r2.lugar = r1.lugar)");
+    		$q->setParameter(1,$id);
+    		return $q->getResult();
   	}
     
     public function getDatosUsuario($usuario) {
-      
+
+        //Total recomendaciones usuario
+        $recomendaciones = $this->getUsuarioRecomendaciones($usuario->getId()); 
+        $totalRecomendaciones = count($recomendaciones);
+
+        //Primeras recomendaciones usuario
+        $primerasRecomendaciones = $this->getPrimerasRecomendaciones($usuario->getId());
+        $totalPrimerasRecomendaciones = count($primerasRecomendaciones);   
+
+        //Total de lugares agregados por el usuario
+        $totalLugaresAgregados = $this->getTotalLugaresAgregadosUsuario($usuario->getId());
+
+        //Total de fotos de lugares agregadas por el usuario
+        $totalImagenesLugar= $this->getTotalFotosLugaresAgregadasUsuario($usuario->getId());
+
+        //Cálculo de edad
+        if($usuario->getFechaNacimiento() != null) {
+            $birthday = $usuario->getFechaNacimiento()->format('d-m-Y');
+            if($birthday != '30-11--0001') {
+                list($d,$m,$Y)    = explode("-",$birthday);
+                $edad = date("md") < $m.$d ? date("Y")-$Y-1 : date("Y")-$Y;
+            }
+            else {
+                $edad = '0';
+            }            
+        } else {
+            $edad = '0';
+        }
+        
+
+        //Nombre del sexo
+        if($usuario->getSexo() != null) {
+            if($usuario->getSexo() == "m") {
+                $sexoResult = "Hombre";
+            }
+            else {
+                $sexoResult = "Mujer";
+            }
+        } else {
+            $sexoResult = null;
+        }
+
+        //Array con links de usuario
+        $links = array();
+        if($usuario->getWeb() != null || $usuario->getWeb() != '') {
+            $links[] = $usuario->getWeb();
+        }
+        if($usuario->getFacebook() != null || $usuario->getFacebook() != '') {
+            $links[] = $usuario->getFacebook();
+        }
+        if($usuario->getTwitter() != null || $usuario->getTwitter() != '') {
+            $links[] = $usuario->getTwitter();
+        }
+        
+        /*
+         *  Armado de Datos para pasar a Twig
+         */
+        $data = $usuario;
+        $data->totalRecomendaciones = $totalRecomendaciones;
+        $data->totalPrimerasRecomendaciones = $totalPrimerasRecomendaciones;
+        $data->totalLugaresAgregados = $totalLugaresAgregados['total'];
+        $data->totalImagenesLugar = $totalImagenesLugar['total'];
+        $data->edadResult = $edad;
+        $data->sexoResult = $sexoResult;
+        $data->desdeResult = $usuario->getFechaRegistro()->format('d-m-Y');
+        $data->links = $links;
+
+        return $data;
     }	
 }
