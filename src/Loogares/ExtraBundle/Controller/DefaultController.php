49f5c1f6ca4283cd7d77104f934d0be090dae689
Sebastián Vicencio <sivicencio@gmail.com>
2012-02-17 14:36:42 -0300

    Implementando sistema Ad Manager

--- src/Loogares/ExtraBundle/Controller/DefaultController.php
+++ src/Loogares/ExtraBundle/Controller/DefaultController.php
@@ -3,13 +3,14 @@
 namespace Loogares\ExtraBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Loogares\LugarBundle\Entity\TipoCategoria;
 
 
 class DefaultController extends Controller
 {
     
-    public function sindexAction($name)
+    public function indexAction($name)
     {
         return $this->render('LoogaresExtraBundle:Default:index.html.twig', array('name' => $name));
     }
@@ -47,14 +48,26 @@ class DefaultController extends Controller
         return $this->render('::ciudad.html.twig', array('ciudades' => $data));
     }
 
-    public function localeAction($slug) {
-        $em = $this->getDoctrine()->getEntityManager();
-        $cr = $em->getRepository("LoogaresExtraBundle:Ciudad");
-        $ciudad = $cr->findOneBySlug($slug);
+    public function localeAction($slug, $start=null) {
+        if((!$this->get('session')->get('ciudad') && $start) || !$start ) {
+            $em = $this->getDoctrine()->getEntityManager();
+            $cr = $em->getRepository("LoogaresExtraBundle:Ciudad");
+            $ciudad = $cr->findOneBySlug($slug);
 
-        // Seteamos el locale correspondiente a la ciudad en la sesión
-        $this->get('session')->setLocale($ciudad->getPais()->getLocale());        
+            // Seteamos el locale correspondiente a la ciudad en la sesión
+            $this->get('session')->setLocale($ciudad->getPais()->getLocale());
 
+            $ciudadArray = array();
+            $ciudadArray['id'] = $ciudad->getId();
+            $ciudadArray['nombre'] = $ciudad->getNombre();
+            $ciudadArray['slug'] = $ciudad->getSlug();
+
+            $this->get('session')->set('ciudad',$ciudadArray);
+        }     
+
+        if($start) {
+            return new Response('');
+        }
         // Redirección a vista de login 
         return $this->redirect($this->generateUrl('login'));
     }
