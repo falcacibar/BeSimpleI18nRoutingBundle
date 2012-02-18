4728b5fd6b12c379407eede0354217746bd5c644
Alberto Marcías <albertomarcias@hotmail.com>
2012-02-18 14:05:24 -0300

    Nuevos diseños de mails

--- src/Loogares/AdminBundle/Controller/AdminController.php
+++ src/Loogares/AdminBundle/Controller/AdminController.php
@@ -1423,8 +1423,8 @@ class AdminController extends Controller
     //    return $this->render('LoogaresAdminBundle:Mails:test_mail_accion_recomendacion.html.twig');
     //    return $this->render('LoogaresLugarBundle:Mails:test_mail_enviar.html.twig');
     //    return $this->render('LoogaresLugarBundle:Mails:test_mail_lugar.html.twig');
-        return $this->render('LoogaresLugarBundle:Mails:test_mail_recomendar.html.twig');
-    //    return $this->render('LoogaresLugarBundle:Mails:test_mail_reporte.html.twig');
+    //    return $this->render('LoogaresLugarBundle:Mails:test_mail_recomendar.html.twig');
+        return $this->render('LoogaresLugarBundle:Mails:test_mail_reporte.html.twig');
     //    return $this->render('LoogaresUsuarioBundle:Mails:test_mail_olvidar_password.html.twig');
     //    return $this->render('LoogaresUsuarioBundle:Usuarios:test_mail_borrar_cuenta.html.twig');
     //    return $this->render('LoogaresUsuarioBundle:Usuarios:test_mail_registro.html.twig');
