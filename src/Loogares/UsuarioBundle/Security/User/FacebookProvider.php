4c49d104e7e02092b5ef46eb09a21a416bcf5692
Sebastián Vicencio <sivicencio@gmail.com>
2012-02-27 23:11:41 -0300

    Facebook Provider

---
+++ src/Loogares/UsuarioBundle/Security/User/FacebookProvider.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Loogares\UsuarioBundle\Security\User;
+
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Loogares\UsuarioBundle\Entity\Usuario;
+use \BaseFacebook;
+use \FacebookApiException;
+
+class FacebookProvider implements UserProviderInterface
+{
+    /**
+     * @var \Facebook
+     */
+    protected $facebook;
+    protected $userManager;
+    protected $validator;
+
+    public function __construct(BaseFacebook $facebook, $userManager, $validator)
+    {
+        $this->facebook = $facebook;
+        $this->userManager = $userManager;
+        $this->validator = $validator;
+    }
+
+    public function supportsClass($class)
+    {
+        return $this->userManager->supportsClass($class);
+    }
+
+    public function findUserByFbId($fbId)
+    {
+        return $this->userManager->findUserBy(array('facebook_uid' => $fbId));
+    }
+
+    public function loadUserByUsername($username)
+    {
+        $em = $this->getEntityManager();
+
+        // Buscamos por UID para ver si existe en nuestra DB
+        $user = $this->findUserByFbId($username);
+
+        try {
+            $fbdata = $this->facebook->api('/me');
+        } catch (FacebookApiException $e) {
+            $fbdata = null;
+        }
+
+        if (!empty($fbdata)) {
+            if (empty($user)) {
+                // Revisamos si un usuario con el mismo email está registrado (para quienes conectan estando registrados)
+                if (isset($fbdata['email'])) {
+                    $user = $this->userManager->findUserByMail($fbdata['email']);
+                }
+                
+                // Si en este punto el usuario no existe, entonces debemos registrarlo
+                if (empty($user)) {
+                    $user = new Usuario();
+                    $user->setPassword('');
+
+                    if (isset($fbdata['email'])) {
+                        $user->setMail($fbdata['email']);
+                    }
+                    $em->persist($user);
+                }
+
+                $user->setFBData($fbdata);               
+            }           
+
+            /*if (count($this->validator->validate($user, 'Facebook'))) {
+                // TODO: the user was found obviously, but doesnt match our expectations, do something smart
+                throw new UsernameNotFoundException('The facebook user could not be stored');
+            }*/
+            $em->flush();
+        }
+
+        if (empty($user)) {
+            throw new UsernameNotFoundException('The user is not authenticated on facebook');
+        }
+
+        return $user;
+    }
+
+    public function refreshUser(UserInterface $user)
+    {
+        if (!$this->supportsClass(get_class($user)) || !$user->getFacebookUid()) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', get_class($user)));
+        }
+
+        return $this->loadUserByUsername($user->getFacebookUid());
+    }
+}
