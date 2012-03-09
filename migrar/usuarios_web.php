1d98735879a983365b8d4f72e76b57288ad61f1c
Iago Sanjurjo <i_sanjurjo@yahoo.es>
2012-03-09 15:41:13 -0300

    Migrar web-twitter-fb

---
+++ migrar/usuarios_web.php
@@ -0,0 +1,40 @@
+<?php
+include('config.php');
+
+$STH = $LBH->query('SELECT Id, Link_1, Link_2, Link_3 FROM `Usuario`');
+$STH->setFetchMode(PDO::FETCH_ASSOC);
+
+
+while($row = $STH->fetch()){
+    $final= array(
+        'web' => NULL
+        ,'facebook' => NULL
+        ,'twitter' => NULL);
+
+    foreach($row as $key => $value){
+        if($key != 'Id') {
+            //Comprobamos Link_1
+            if(strpos($value, "facebook") !== FALSE){
+                //echo ' Si';
+                $final['facebook']=$value;
+                
+            }elseif(strpos($value, "twitter") !== FALSE){
+                //Si
+                $final['twitter']=$value;
+                
+            }elseif($value!==null){
+                $final['web']=$value;
+            }
+        }
+    }
+
+    //insert
+    
+    $update= "UPDATE Usuario 
+            SET Link_1='".$final['web']."', Link_2='".$final['facebook']."', Link_3='".$final['twitter']."'
+            WHERE Id=".$row['Id'];
+    echo "$update </br>";
+    $LBH->exec($update);
+    
+}
+?>
