b55b67d34c3247818adf3899be5e87bee50e07da
Tomás Mayr <akutsito@gmail.com>
2012-03-13 02:16:40 -0400

    Error del blog

--- src/Loogares/BlogBundle/Controller/DefaultController.php
+++ src/Loogares/BlogBundle/Controller/DefaultController.php
@@ -14,13 +14,18 @@ class DefaultController extends Controller
 
     public function postAction($slug){
     	$em = $this->getDoctrine()->getEntityManager();
+        $anteriores = null;
+        
         $pr = $em->getRepository("LoogaresBlogBundle:Posts");
         $post = $pr->findOneBySlug($slug);
-        $q = $em->createQuery('SELECT u FROM Loogares\BlogBundle\Entity\Posts u WHERE u.tipo_post = ?1 and u.lugar = ?2 and u.id != ?3');
-        $q->setParameter(1, $post->getTipoPost());
-        $q->setParameter(2, $post->getLugar());
-        $q->setParameter(3, $post->getId());
-        $anteriores = $q->getResult();
+
+        if(gettype($post) == 'object'){
+            $q = $em->createQuery('SELECT u FROM Loogares\BlogBundle\Entity\Posts u WHERE u.tipo_post = ?1 and u.lugar = ?2 and u.id != ?3');
+            $q->setParameter(1, $post->getTipoPost());
+            $q->setParameter(2, $post->getLugar());
+            $q->setParameter(3, $post->getId());
+            $anteriores = $q->getResult();
+        }
 
         return $this->render('LoogaresBlogBundle:Default:post.html.twig', array('post' => $post, 'anteriores' => $anteriores));
     }
