504aefa176e14fb4151c6b86f77aaffa65dd204e
Sebastián Vicencio <sivicencio@gmail.com>
2012-03-19 19:05:51 -0300

    Avances en foto de facebook al registrar

--- src/Loogares/UsuarioBundle/Security/User/FacebookProvider.php
+++ src/Loogares/UsuarioBundle/Security/User/FacebookProvider.php
@@ -98,7 +98,7 @@ class FacebookProvider implements UserProviderInterface
                         ));
                     }
                     
-                    $user->setImagenFull($fbimg[0]['pic_large']);
+                    $user->setImagenFull($fbimg[0][0]);
                     $user->setFechaRegistro(new \DateTime());
                     $user->setNewsletterActivo(1);
                     $hashConfirmacion = md5($user->getMail().time());
