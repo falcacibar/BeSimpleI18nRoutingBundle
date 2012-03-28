5063601de1ff39de93a6707f81a4264090eb61eb
Tomás Mayr <akutsito@gmail.com>
2012-03-28 14:09:36 -0400

    fix

--- src/Loogares/ExtraBundle/Controller/DefaultController.php
+++ src/Loogares/ExtraBundle/Controller/DefaultController.php
@@ -170,7 +170,6 @@ class DefaultController extends Controller
             }else{
                 $entidad->relativo = $trr->tiempoRelativo($entidad->getFechaCreacion()->format('Y-m-d H:i:s'));
             }
-            $a->ent = $entidad;
             $actividad[$i]->ent = $entidad;
         }
 
