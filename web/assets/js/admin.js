b9a94ce6b177b3190583ac8e2ccad5ae7b57b504
Sebastián Vicencio <sivicencio@gmail.com>
2012-06-04 03:02:32 -0400

    Timepicker para fecha de inicio y término de concurso

--- web/assets/js/admin.js
+++ web/assets/js/admin.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
+    // Configuración de datepicker
     $.datepicker.setDefaults( $.datepicker.regional[ "es" ] );
-    var dates = $( "#fecha-desde-ph, #fecha-hasta-ph, #fecha-desde-c-ph" ).datepicker({
+    var dates = $( "#fecha-desde-ph, #fecha-hasta-ph" ).datepicker({
         defaultDate: "+1w",
         changeMonth: true,
         changeYear: true,
         yearRange: '2009:c',
         numberOfMonths: 1,
         onSelect: function( selectedDate ) {
-            if(this.id == 'fecha-desde-c-ph'){
+            if(this.id == 'fecha-desde-ph'){
                 var option = 'minDate';
             }else if(this.id == 'fecha-hasta-ph'){
                 var option = 'maxDate';
@@ -26,11 +27,38 @@ $(document).ready(function(){
     $( "#fecha-desde-ph" ).datepicker( "option", "altFormat", "dd-mm-yy" );
     $( "#fecha-desde-ph" ).datepicker( "option", "altField", '#fecha-desde' );
 
-    $( "#fecha-desde-c-ph" ).datepicker( "option", "altFormat", "dd-mm-yy" );
-    $( "#fecha-desde-c-ph" ).datepicker( "option", "altField", '#fecha-desde-c' );
-
     $( "#fecha-hasta-ph" ).datepicker( "option", "altFormat", "dd-mm-yy" );
     $( "#fecha-hasta-ph" ).datepicker( "option", "altField", '#fecha-hasta' );
+
+    // Configuración de timepicker
+    var datetimes = $( "#fecha-desde-c-ph, #fecha-hasta-c-ph" ).datetimepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        changeYear: true,
+        yearRange: '2009:c',
+        numberOfMonths: 1,
+        onSelect: function( selectedDate ) {
+            if(this.id == 'fecha-desde-c-ph'){
+                var option = 'minDate';
+            }else if(this.id == 'fecha-hasta-c-ph'){
+                var option = 'maxDate';
+            }
+            console.log($(this).data( "datepicker" ));
+            var instance = $( this ).data( "datepicker" ),
+                date = $.datepicker.parseDate(
+                    instance.settings.dateFormat ||
+                    $.datepicker._defaults.dateFormat,
+                    selectedDate, instance.settings );
+            datetimes.not( this ).datepicker( "option", option, date );
+        },
+        dateFormat: 'dd-mm-yy',
+    });
+
+    $( "#fecha-desde-c-ph" ).datetimepicker( "option", "altFormat", "dd-mm-yy" );
+    $( "#fecha-desde-c-ph" ).datetimepicker( "option", "altField", '#fecha-desde-c' );
+
+    $( "#fecha-hasta-c-ph" ).datetimepicker( "option", "altFormat", "dd-mm-yy" );
+    $( "#fecha-hasta-c-ph" ).datetimepicker( "option", "altField", '#fecha-hasta-c' );
 
     $('form').submit(function(){
         $(this).find('input, select').each(function(){
--- web/assets/js/jquery.ui.datepicker-es.js
+++ web/assets/js/jquery.ui.datepicker-es.js
@@ -20,4 +20,32 @@ jQuery(function($){
 		showMonthAfterYear: false,
 		yearSuffix: ''};
 	$.datepicker.setDefaults($.datepicker.regional['es']);
+
+	$.timepicker.regional['es'] = {
+		closeText: 'Listo',
+		prevText: '&#x3c;Ant',
+		nextText: 'Sig&#x3e;',
+		currentText: 'Hoy',
+		monthNames: ['Enero','Febrero','Marzo','Abril','Mayo','Junio',
+		'Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
+		monthNamesShort: ['Ene','Feb','Mar','Abr','May','Jun',
+		'Jul','Ago','Sep','Oct','Nov','Dic'],
+		dayNames: ['Domingo','Lunes','Martes','Mi&eacute;rcoles','Jueves','Viernes','S&aacute;bado'],
+		dayNamesShort: ['Dom','Lun','Mar','Mi&eacute;','Juv','Vie','S&aacute;b'],
+		dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','S&aacute;'],
+		weekHeader: 'Sm',
+		dateFormat: 'dd/mm/yy',
+		timeFormat: 'hh:mm tt',
+		firstDay: 1,
+		isRTL: false,
+		showMonthAfterYear: false,
+		yearSuffix: '',
+		timeOnlyTitle: 'Elegir horario',
+		timeText: 'Horario',
+		hourText: 'Hora',
+		minuteText: 'Minuto',
+		secondText: 'Segundo',
+		millisecText: 'Milisegundo',
+		timezoneText: 'Zona horaria'};
+	$.timepicker.setDefaults($.timepicker.regional['es']);
 });
