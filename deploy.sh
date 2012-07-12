f893f8defe10a900a86c604da6c2bd838fcc3d22
Tomas Mayr <tmayr@loogares.chilish.com>
2012-07-12 10:30:02 -0400

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-sudo chown -R 777 $USER:loogares /var/www/loogares.com/loogares
+sudo chown -R $USER:loogares /var/www/loogares.com/loogares
 cd /var/www/loogares.com/loogares && git pull origin master
 sudo chmod -R 777 app/cache
 sudo chmod -R 777 app/logs
